c5677cefaeeea00b07f87495d9083a23380e3853
MemDawg26 <149435079+MemDawg26@users.noreply.github.com>
2023-11-08 14:25:01 -0600

    Update inventory.py

---
+++ inventory.py
@@ -0,0 +1,12 @@
+class Inventory:
+    def Inventory():
+
+    def Inventory(databaseName, tableName):
+
+    def viewInventory():
+        #
+    def searchInventory():
+        title = intput("Please enter a title: ")
+        SELECT from databaseName where title = "%" + title + "%"
+
+    def decreaseStock(ISBN):
