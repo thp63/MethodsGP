471dba9d93d49d05f158c3ddfd2f59b72d1995c6
Tyner Parker <149347209+thp63@users.noreply.github.com>
2023-11-08 14:42:12 -0600

    Add files via upload

--- Cart.py
+++ Cart.py
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-
-class Cart:
-
-    def Cart():
-        return
-    
-    def Cart(databaseName, tableName):
-        return
-
-    def viewCart(userID, inventoryDatabase):
-        for item in Cart:
-            print(item)
-        return
-
-    def addToCart(userID, ISBN):
-        return
-
-    def removeFromCart(userID, ISBN):
-        return
-
-    def checkOut(userID):
-        return
-=======
 
 class Cart:
 
@@ -42,4 +18,3 @@ class Cart:
 
     def checkOut(userID):
         return
->>>>>>> c5677cefaeeea00b07f87495d9083a23380e3853
