f488e448061bac4f00b40ec08aa098f306d9931f
Tyner Parker <thp63@msstate.edu>
2023-11-14 16:45:37 -0600

    Adding menu to main

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
+#Menu Function
 def menu():
-    while 1:
-        login = input("Login: ")
-        print()
+    print("MethodsGP Menu")
+    print("(0) Exit")
+    print("(1) Login")
+    print("(2) Create Account")
+    print("(3) Logout")
+
+#Variables
+
+
+#Main
+menu()
+option = int(input("Option (0-3): "))
+
+#Menu Loop
+while option != 0:
+    if option == 1:
+        print("Logging in...")
+    elif option == 2:
+        print("Creating account...")
+    elif option == 3:
+        print("Logging out...")
+        break
+    else:
+        print("Invalid option! Try again!")
+        option = int(input("Option (1-3): "))
