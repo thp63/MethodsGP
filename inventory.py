b3d2de500d46726a9d7fecb0b4abb9f81e081f61
MemDawg26 <149435079+MemDawg26@users.noreply.github.com>
2023-11-17 14:09:53 -0600

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -1,12 +1,23 @@
-class Inventory:
-    def Inventory():
+import sqlite3
 
-    def Inventory(databaseName, tableName):
+class Inventory:
+    def __init__(self, databaseName = 0, tableName = 0):
+        self.databaseName = databaseName
+        self.tableName = tableName
 
     def viewInventory():
-        #
-    def searchInventory():
-        title = intput("Please enter a title: ")
+        connection = sqlite3.connect("MethodsGPDB.db")
+        cursor = conncection.cusor()
+        cursor.execute("SELECT * FROM Inventory")
 
-    def decreaseStock(ISBN):
-        return
+    def searchInventory():
+        connection = sqlite3.connect("MethodsGPDB.db")
+        cursor = conncection.cusor()
+        answer = yes
+        while answer == "yes":
+            title = input("Enter the title for the book you are looking for:")
+            cursor.execute("SELECT * FROM Inventory WHERE Title = %" + title + "%")
+            answer = input("Would you like to search again?(yes/no): ")
+
+    def decreaseStock(self, ISBN):
+        
