c7b3e6b6525254edf455d6955abb77580105c549
conner-roberson <clr655@msstate.edu>
2023-11-25 18:07:44 -0600

    User Class work on Login

--- user.py
+++ user.py
@@ -1,3 +1,6 @@
+import sqlite3
+
+
 class User:
     def _init_(self):
         self.userId =""
@@ -6,25 +9,43 @@ class User:
         self.dbName = dbName
         self.tableName = tableName
     def login(self):
-        pass
+        self.userId = input("User ID: ")
+        self.password = input("User ID: ")
+        connection = sqlite3.connect("MethodsGPDB.db")
+        cursor = connection.cursor()
+        cursor.execute("SELECT UserID, Password FROM User WHERE UserID=" + self.userId + ", Password=" + self.password)
+        result = cursor.fetchall()
+        #ID result from db
+        DBID = result[0][0]
+        #Pass result from db
+        DBPass = result[0][1]
+        if DBID != self.userId or DBPass != self.password:
+            if DBID != self.userId:
+                print("User ID not found")
+            if DBPass != self.password:
+                print("Incorrect Pass")
+        else:
+            self.loggedIn = True
     def logout(self):
         pass
     def viewAccountInformation(self):
         pass
     def createAccount(self):
         pass
-    def getLoggedIn(self):
-        pass
-    def setUserId(self, value):
-        self.setUserId = value
+    def setPassword(self):
+        self.password = input("Password: ")
+    def getPassword(self):
+        return self.password
+    def setUserId(self):
+        self.setUserId = input("User ID: ")
     def getUserId(self):
         return self.userId
-    def setdbName(self, value):
-        self.dbName = value
+    def setdbName(self):
+        self.dbName
     def getdbName(self):
         return self.dbName
-    def setTableName(self, value):
-        self.tableName = value
+    def setTableName(self):
+        self.tableName
     def getTableName(self):
         return self.tableName
     
