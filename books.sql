0c4f4f4d48b6a32017f2311347bf2eed9c8a4e4b
conner-roberson <clr655@msstate.edu>
2023-11-29 11:27:38 -0600

    add books sql to populate db

---
+++ books.sql
@@ -0,0 +1,8 @@
+INSERT INTO `books` (`ISBN`, `Title`, `Author`, `Year`, `Genre`) VALUES
+('978-0156028356', 'The Color Purple', 'Alice Walker', 1982, 'Epistolary'),
+('978-0307265432', 'The Road', 'Cormac McCarthy', 2006, 'Post apocalyptic fiction'),
+('978-0312424404', 'Gilead', 'Marilynne Robinson', 2004, 'Novel'),
+('978-0446310789', 'To Kill a Mockingbird', 'Harper Lee', 1960, 'Southern Gothic'),
+('978-0451524935', '1984', 'George Orwell', 1949, 'Dystopian'),
+('978-1400033416', 'Beloved', 'Toni Morrison', 1987, 'American Literature'),
+('978-1501156748', 'Misery', 'Stephen King', 1987, 'Psychological Horror');
