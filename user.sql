ee2ff1c97a31690892dd2a4188098acefcdffe6b
conner-roberson <clr655@msstate.edu>
2023-11-29 11:26:08 -0600

    create user sql file to populate db

---
+++ user.sql
@@ -0,0 +1,8 @@
+INSERT INTO User (userID, email, password, firstName, lastName, address, city, state, zip, payment)
+VALUES ('clr655', 'clr655@msstate.edu', 'Conner655', 'Conner', 'Roberson', '56 Black Jack', 'Starkville', '39759', 'Visa');
+
+INSERT INTO User (userID, email, password, firstName, lastName, address, city, state, zip, payment)
+VALUES ('eml335', 'eml335@msstate.edu', 'Ethan335', 'Ethan', 'Lipke', '78 Black Jack', 'Starkville', '39759', 'PayPal');
+
+INSERT INTO User (userID, email, password, firstName, lastName, address, city, state, zip, payment)
+VALUES ('thp63', 'thp63@msstate.edu', 'thp63', 'Tyner', 'Parker', '38 Black Jack', 'Starkville', '39759', 'American Express');
